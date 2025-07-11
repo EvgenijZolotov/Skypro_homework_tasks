485f070bc111117ea8409785f89ae7e14a6f99c8
Cowboy <evgenij.zolotov@mail.ru>
2025-07-11 19:24:53 +0300

    Исправил ошибки в lesson_1_task_3.py, lesson_1_task_4.py

--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,3 +1,4 @@
-first_name = input("Евгений ")
-last_name = input("Золотов ")
-print("Вас зовут: ", last_name, first_name)
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
+
+print("Вас зовут:", last_name, first_name)
--- lesson_01/lesson_1_task_4.py
+++ lesson_01/lesson_1_task_4.py
@@ -1,3 +1,5 @@
-def print_greeting():
-    print("Привет мир!")
-    print_greeting()
+def print_greeting(a):
+    print(a)
+
+
+print_greeting("Привет, мир!")

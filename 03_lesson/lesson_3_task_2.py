feeea10caeff9630917597246280fc19fb8685b1
EvgenijZolotov <47779295+EvgenijZolotov@users.noreply.github.com>
2025-08-05 11:59:07 +0300

    Add files via upload

---
+++ 03_lesson/lesson_3_task_2.py
@@ -0,0 +1,12 @@
+from smartphone import Smartphone
+
+catalog = [
+    Smartphone("Apple", "iPhone 15", "+79111234567"),
+    Smartphone("Samsung", "Galaxy S23", "+79219876543"),
+    Smartphone("Xiaomi", "Redmi Note 12", "+79335557788"),
+    Smartphone("Google", "Pixel 7", "+79441112233"),
+    Smartphone("OnePlus", "11 Pro", "+79559998877")
+]
+
+for phone in catalog:
+    print(phone)

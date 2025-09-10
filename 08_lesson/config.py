90ad2480f4ec4e53d228f36b9a170d4a518b8d69
Cowboy <evgenij.zolotov@mail.ru>
2025-09-10 10:32:10 +0300

    Сохранил все незавершённые изменения

--- 08_lesson/config.py
+++ 08_lesson/config.py
@@ -1,11 +1,6 @@
-# config.py
+import os
+from dotenv import load_dotenv
 
-API_BASE_URL = (
-    "https://an12-acc1.yougile.com/data/"
-    "company-link-register-pending"
-)
+load_dotenv()
 
-API_TOKEN = (
-    "1757003396535_d0270794fd9b3149d598b42db8364d30"
-    "ff7978dee465f911c2bd2d9b6d1e41e0"
-)
+API_TOKEN = os.getenv("API_TOKEN")
